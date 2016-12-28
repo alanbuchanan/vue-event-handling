c26ed0a80d331b2b4a22452744321fa4eb084aff
Rory Smith <rory.smith@korelogic.co.uk>
2016-12-28 17:27:30 +0000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <button @mouseenter="counter += 1">Increment the count!</button>
+    <button @click="increment()">Increment using a method!</button>
+    <div v-text="counter"></div>
+    <form @submit.prevent="logName()">
+      <input @keyup.ctrl.alt.shift.down="keyHandler()" v-model="firstName" placeholder="First name..." />
+    </form>
+  </div>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        counter: 0,
+        firstName: ''
+      }
+    },
+
+    methods: {
+      increment() {
+        console.log('counter incremented!')
+        this.counter += 1
+      },
+
+      logName() {
+        console.log('firstName:', this.firstName)
+      },
+
+      keyHandler() {
+        this.firstName = 'Evan'
+      }
+    }
+  }
+</script>
